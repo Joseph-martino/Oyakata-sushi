097c353d3073f2de9986a28daed3a6c144c140eb
Joseph-martino <71452904+Joseph-martino@users.noreply.github.com>
2024-06-04 12:52:57 +0200

    reservation confirmation, and reservation number added

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Reservation } from '../models/Reservation';
+import { Observable, catchError, of, switchMap, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,34 +9,53 @@ import { Reservation } from '../models/Reservation';
 export class ReservationService {
 
   reservation!: Reservation;
+  reservationNumber!: number;
 
   constructor(private http: HttpClient) { }
 
 
-  createReservation(formValue: {firstName: string, familyName: string, email: string, numberOfPersons: number, reservationDateString: string} ){
+  createReservation(formValue: {firstName: string, familyName: string, email: string, numberOfPersons: number, reservationDateString: string}): Observable<Reservation> {
+    return this.getNumberTotalOfReservations().pipe(
+        switchMap(reservationNumber => {
+            this.reservationNumber = reservationNumber +1;
+            console.log(this.reservationNumber);
 
-    this.reservation = new Reservation();
+            this.reservation = new Reservation();
+            this.reservation.firstName = formValue.firstName;
+            this.reservation.familyName = formValue.familyName;
+            this.reservation.reservationNumber = `N°${this.reservationNumber.toString()}`;
+            this.reservation.email = formValue.email;
+            this.reservation.numberOfPersons = formValue.numberOfPersons;
 
-    console.log( "format:" + formValue.reservationDateString);
+            const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
+            if (dateRegex.test(formValue.reservationDateString)) {
+                this.reservation.reservationDate = new Date(formValue.reservationDateString);
+            } else {
+                console.error("Invalid date format: ", formValue.reservationDateString);
+                // Gérer l'erreur de format de date ici
+            }
 
-    this.reservation.firstName = formValue.firstName;
-    this.reservation.familyName = formValue.familyName;
-    this.reservation.reservationNumber = "1";
-    this.reservation.email = formValue.email;
-    this.reservation.numberOfPersons = formValue.numberOfPersons;
-        // Vérification de la validité de la chaîne de caractères de date
-        const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
-        if (dateRegex.test(formValue.reservationDateString)) {
-            // Conversion de la chaîne de caractères en objet Date
-            this.reservation.reservationDate = new Date(formValue.reservationDateString);
-        } else {
-            console.error("Invalid date format: ", formValue.reservationDateString);
-            // Gérer l'erreur de format de date ici
-        }
+            console.log(this.reservation);
 
-    console.log(this.reservation);
+            return this.http.post<Reservation>('http://localhost:8080/core/rest/reservation/create', this.reservation);
+        })
+    );
+}
 
-    return this.http.post<Reservation>('http://localhost:8080/core/rest/reservation/create', this.reservation);
 
+  getNumberTotalOfReservations(): Observable<number>{
+    return this.http.get<number>('http://localhost:8080/core/rest/reservation/total').pipe(
+      tap((totalMenuNumber) => this.logInfo(totalMenuNumber)),
+      catchError((error) => this.handleError(error, undefined))
+    );
+  }
+
+  private logInfo(response: any){
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: any){
+    console.error(error);
+    return of(errorValue);
   }
 }
